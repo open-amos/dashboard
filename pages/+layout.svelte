edcb45e07cab952c0cd34da5de5bb25e2edb167a
bjmrey <benjaminrey31@msn.com>
2025-11-07 09:34:30 +0100

    Fix logo path

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -2,11 +2,14 @@
 	import '@evidence-dev/tailwind/fonts.css';
 	import '../app.css';
 	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	import { base } from '$app/paths';
 	export let data;
 </script>
 
-<EvidenceDefaultLayout {data} logo="/amos.png" 
-							  neverShowQueries={true}
-							  githubRepo="https://github.com/open-amos"> 
+<EvidenceDefaultLayout
+	{data}
+	logo={`${base}/amos.png`}
+	neverShowQueries={true}
+	githubRepo="https://github.com/open-amos">
 	<slot slot="content" />
-</EvidenceDefaultLayout> 
+</EvidenceDefaultLayout>
